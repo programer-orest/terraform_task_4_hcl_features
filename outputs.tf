5e8663d73e23e336cd949100ac7e7885b7a055f0
Orest Yakymiv <akimivorest@gmail.com>
2025-06-24 13:52:25 +0300

    test

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "name_machine" {
 }
 
 output "tag_machine" {
-  value = join(", ", [for tag in azurerm_virtual_machine.main.* : join(", ", [for k, v in tag.tags : "${k}=${v}"])])
+  value = join(", ", [for tag in azurerm_virtual_machine.main : join(", ", [for k, v in tag.tags : "${k}=${v}"])])
 }
 
 output "id_machine" {
-  value = [for vm in azurerm_virtual_machine.main.* : vm.id]
+  value = [for vm in azurerm_virtual_machine.main : vm.id]
 }
